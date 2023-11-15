9017f465b7814f6b6b7538680c41c0617c1e76e1
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2023-11-15 22:14:18 +0800

    feature delete chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -64,11 +64,13 @@ class ChirpController extends Controller
         return redirect(route('chirps.index'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Chirp $chirp)
+
+    public function destroy(Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
     }
 }
